61145513c9b3552a02ffcdce15a76e48c90d4647
Yusuf <zxces@vk.com>
2023-12-14 22:31:49 +0000

    documentation

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -4,17 +4,46 @@ from django.contrib.auth.hashers import make_password
 
 
 class PlayerAchievesSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели PlayerAchieves.
+
+    :param Meta: Класс метаданных для указания модели и полей.
+    :type Meta: class
+    :raises ValidationError: Если данные не соответствуют ожидаемому формату.
+    """
     class Meta:
         model = PlayerAchieves
         fields = '__all__'
 
 
 class PlayerSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Player.
+
+    :param login: Логин игрока.
+    :type login: str
+    :param password: Пароль игрока.
+    :type password: str
+    :param achieves: Сериализатор ачивок игрока.
+    :type achieves: PlayerAchievesSerializer
+    :param Meta: Класс метаданных для указания модели и полей.
+    :type Meta: class
+    :raises ValidationError: Если данные не соответствуют ожидаемому формату.
+    """
     login = serializers.CharField(max_length=30)
     password = serializers.CharField()
     achieves = PlayerAchievesSerializer(required=False)
 
     def create(self, validated_data):
+        """
+        Метод для создания нового игрока.
+
+        :param validated_data: Валидированные данные для создания игрока.
+        :type validated_data: dict
+        :return: Объект игрока.
+        :rtype: myapp.models.Player
+        :raises ValidationError: Если данные не соответствуют ожидаемому формату.
+        """
         player_achieves_data = validated_data.pop('achieves', None)
         achieves_instance = PlayerAchieves.objects.create(**player_achieves_data)
 
--- myapp/views.py
+++ myapp/views.py
@@ -10,19 +10,56 @@ from django.contrib.auth.hashers import check_password
 
 
 def index(request):
+    """
+    Отображает главную страницу приложения.
+
+    :param request: Объект запроса Django.
+    :type request: django.http.HttpRequest
+    :return: Объект ответа Django с отображением главной страницы.
+    :rtype: django.http.HttpResponse
+    """
     return render(request, 'myapp/index.html')
 
 
 class PlayerViewSet(ModelViewSet):
+    """
+    Представление для взаимодействия с моделью Player.
+
+    :param serializer_class: Класс сериализатора для модели Player.
+    :type serializer_class: serializers.PlayerSerializer
+    :param queryset: Запрос для получения всех объектов модели Player.
+    :type queryset: django.db.models.query.QuerySet
+    :param lookup_field: Поле для поиска объекта модели Player.
+    :type lookup_field: str
+        """
     serializer_class = serializers.PlayerSerializer
     queryset = models.Player.objects.all()
     lookup_field = 'login'
 
 
 class ItemAPIView(APIView):
+    """
+    API-представление для взаимодействия с объектами Player.
+
+    :param serializer_class: Класс сериализатора для модели Player.
+    :type serializer_class: serializers.PlayerSerializer
+        """
     serializer_class = serializers.PlayerSerializer
 
     def get(self, request, login, password):
+        """
+        Обработчик GET-запроса для проверки пароля игрока.
+
+        :param request: Объект запроса Django.
+        :type request: django.http.HttpRequest
+        :param login: Логин игрока.
+        :type login: str
+        :param password: Пароль игрока.
+        :type password: str
+        :return: Ответ с результатом проверки пароля.
+        :rtype: rest_framework.response.Response
+        :raises Http404: Если параметр login не указан.
+            """
         try:
             player = get_object_or_404(models.Player, login=login)
             is_valid_password = check_password(password, player.password)
@@ -36,6 +73,15 @@ class ItemAPIView(APIView):
             return Response({'error': 'Параметр login не указан'}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request):
+        """
+        Обработчик POST-запроса для создания нового игрока.
+
+        :param request: Объект запроса Django.
+        :type request: django.http.HttpRequest
+        :return: Ответ с результатом создания игрока.
+        :rtype: rest_framework.response.Response
+        :raises serializers.ValidationError: Если данные некорректны.
+            """
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -43,7 +89,36 @@ class ItemAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UpdateAchievesView(APIView):
+    """
+    API-представление для обновления ачивок игрока.
+
+    :param request: Объект запроса Django.
+    :type request: django.http.HttpRequest
+    :param login: Логин игрока.
+    :type login: str
+    :param achieve_type: Тип ачивки (experience, health, points, completion_time).
+    :type achieve_type: str
+    :param type_value: Значение, на которое нужно обновить ачивку.
+    :type type_value: str
+        """
     def patch(self, request, login, achieve_type, type_value):
+        """
+        Обработчик PATCH-запроса для обновления ачивок игрока.
+
+        :param request: Объект запроса Django.
+        :type request: django.http.HttpRequest
+        :param login: Логин игрока.
+        :type login: str
+        :param achieve_type: Тип ачивки (experience, health, points, completion_time).
+        :type achieve_type: str
+        :param type_value: Значение, на которое нужно обновить ачивку.
+        :type type_value: str
+        :return: Ответ с результатом обновления ачивок.
+        :rtype: rest_framework.response.Response
+        :raises PermissionDenied: Если отсутствует разрешение для выполнения запроса.
+        :raises models.Player.DoesNotExist: Если игрок не найден.
+        :raises serializers.ValidationError: Если передан недопустимый тип ачивки.
+            """
         try:
             param_value = request.query_params.get('key')
             if param_value != "f@?2R{yPCZuI2!u(iE!4$Z&(}.sd;G9e4*<kd{D8ltAfs9HNqIR*0w=^#yG^):{?":
